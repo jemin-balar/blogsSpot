6ce12a9cde01ba33f7bc023079d2dbc69f6f2517
Jemin_balar <124352839+jemin-balar@users.noreply.github.com>
2024-02-06 23:47:38 +0530

    Update index.js

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Link from "next/link";
+import Image from 'next/image'
 import { formatDateToMM, truncateText } from "@/utils/utils";
 
 const Blogs = ({ blogs }) => {
@@ -8,43 +9,43 @@ const Blogs = ({ blogs }) => {
     <main className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -mx-4 -my-8">
-          <div class="flex flex-wrap m-2">
+          <div className="flex flex-wrap m-2">
             {blogs?.map((item) => {
               return (
-                <div class="p-4 md:w-1/3">
-                  <div class="h-full border-2 relative border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <div className="p-4 md:w-1/3">
+                  <div className="h-full border-2 relative border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                     {item?.yoast_head_json?.og_image?.length >= 0 && (
-                      <img
-                        class="lg:h-48 md:h-36 w-full object-fill"
+                      <Image
+                        className="lg:h-48 md:h-36 w-full object-fill"
                         src={item?.yoast_head_json?.og_image[0]?.url}
                         alt={item?.yoast_head_json?.og_image[0]?.type}
                       />
                     )}
-                    <div class="p-6">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                    <div className="p-6">
+                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                         Author :
                         <span className="text-green-700">
                           {item?.yoast_head_json?.author}
                         </span>
                       </h2>
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                         Publication At :{" "}
                         <span className="text-green-700">
                           {formatDateToMM(item?.date)}
                         </span>
                       </h2>
-                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                      <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                         {item?.title?.rendered}
                       </h1>
                       <div
-                        class="leading-relaxed mb-3"
+                        className="leading-relaxed mb-3"
                         dangerouslySetInnerHTML={{
                           __html: truncateText(item?.excerpt?.rendered, 0, 74),
                         }}
                       ></div>
-                      <div class="flex items-center flex-wrap">
+                      <div className="flex items-center flex-wrap">
                         <Link href={`/blog/${item.id}`}>
-                          <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                          <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                             Read More
                           </button>
                         </Link>
