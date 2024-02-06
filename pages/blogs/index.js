f07c15d2c1948927f4ca3e69cde94d7b3b7e3912
jemin-balar <balarjemin701@gmail.com>
2024-02-06 19:51:19 +0530

    ui and html section slice method added

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,60 +1,60 @@
 import React from "react";
 import axios from "axios";
 import Link from "next/link";
-import { formatDateToDD, formatDateToMM } from "@/utils/utils";
+import { formatDateToMM, truncateText } from "@/utils/utils";
 
 const Blogs = ({ blogs }) => {
   return (
     <main className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -mx-4 -my-8">
-          {blogs?.map((item) => {
-            return (
-              <div className="py-8 px-4 lg:w-1/3" key={item.id}>
-                <div className="h-full flex items-start">
-                  <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
-                    <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">
-                      {formatDateToMM(item?.date)}
-                    </span>
-                    <span className="font-medium text-lg text-gray-800 title-font leading-none">
-                      {formatDateToDD(item?.date)}
-                    </span>
-                  </div>
-                  <div className="flex-grow pl-6">
-                    <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">
-                      Author {item?.author}
-                    </h2>
-                    <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
-                      {item?.title?.rendered}
-                    </h1>
-                    <div
-                      className="leading-relaxed mb-5"
-                      dangerouslySetInnerHTML={{
-                        __html: item?.content?.rendered,
-                      }}
-                    />
-                    <Link href={`/blog/${item.id}`} passHref legacyBehavior>
-                      <span className="text-indigo-500 inline-flex items-center cursor-pointer">
-                        Learn More
-                        <svg
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path d="M5 12h14"></path>
-                          <path d="M12 5l7 7-7 7"></path>
-                        </svg>
-                      </span>
-                    </Link>
+          <div class="flex flex-wrap m-2">
+            {blogs?.map((item) => {
+              return (
+                <div class="p-4 md:w-1/3">
+                  <div class="h-full border-2 relative border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    {item?.yoast_head_json?.og_image?.length >= 0 && (
+                      <img
+                        class="lg:h-48 md:h-36 w-full object-fill"
+                        src={item?.yoast_head_json?.og_image[0]?.url}
+                        alt={item?.yoast_head_json?.og_image[0]?.type}
+                      />
+                    )}
+                    <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        Author :
+                        <span className="text-green-700">
+                          {item?.yoast_head_json?.author}
+                        </span>
+                      </h2>
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        Publication At :{" "}
+                        <span className="text-green-700">
+                          {formatDateToMM(item?.date)}
+                        </span>
+                      </h2>
+                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                        {item?.title?.rendered}
+                      </h1>
+                      <div
+                        class="leading-relaxed mb-3"
+                        dangerouslySetInnerHTML={{
+                          __html: truncateText(item?.excerpt?.rendered, 0, 74),
+                        }}
+                      ></div>
+                      <div class="flex items-center flex-wrap">
+                        <Link href={`/blog/${item.id}`}>
+                          <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Read More
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </main>
--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,20 @@
 export const formatDateToMM = (dateStr) => {
   const date = new Date(dateStr);
-  const options = { month: "short" };
+  const options = { day: "2-digit", month: "short", year: "2-digit" };
   const formattedDate = date.toLocaleDateString("en-GB", options);
   return formattedDate;
 };
 
 export const formatDateToDD = (dateStr) => {
   const date = new Date(dateStr);
-  const options = { day: "2-digit" };
+  const options = {};
   const formattedDate = date.toLocaleDateString("en-GB", options);
   return formattedDate;
+};
+
+export const truncateText = (text, start, end) => {
+  if (!text) return "";
+  if (text.length <= end) return text;
+  const truncatedText = text.slice(start, end) + "...";
+  return truncatedText;
 };
